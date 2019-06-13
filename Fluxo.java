f19d8bdb68c37fd729033c43d1bd6d37e45c0864
Alecxannder-Sonnenberg <alecxannder.jsbf@usp.br>
2019-06-13 17:17:40 -0300

    Update Fluxo.java

--- Fluxo.java
+++ Fluxo.java
@@ -1,4 +1,16 @@
 public class Fluxo {
-	//Vamos ter de implementar uma lista
-	//Fazer insercao e remoçao
+	List<Gasto> dados = new ArrayList<Gasto>(int 1000); //Aloca uma lista tipo ArrayList com 1000
+	Gasto gastoTemp = new Gasto();
+	
+	public void insereLista() {
+		dados.add(gas);
+		//ou dados.add(Gasto gasTemp); ? (Gasto cannot be resolved to a variable)
+		return;
+	}
+	//em trabalho
+	public void removeLista() {
+		dados.sort(compare(int
+		
+	}
+	
 }
