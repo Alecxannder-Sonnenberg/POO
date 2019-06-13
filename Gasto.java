98d62258922ee70c3149fd102847824e9ca7f920
juliafrare <juliafrare7@gmail.com>
2019-06-13 14:45:52 -0300

    sets e gets adicionados

--- Gasto.java
+++ Gasto.java
@@ -2,12 +2,53 @@ public class Gasto {
 	
 	//Adicionar funcoes set e gets
 	String categoria;
-	String Data; //Procurar se tem uma variavel data (yy/mm/dd)
+	String data; //Procurar se tem uma variavel data (yy/mm/dd)
 	
 	double valor;
 	
 	int id;
 	int parcelas;
+
+	//SETS E GETS
+	public void setCategoria(String novaCategoria){
+		this.categoria = novaCategoria;
+	}
+
+	public String getCategoria(){
+		return categoria;
+	}
+
+	public void setData(String novaData){
+		this.data = novaData;
+	}
+
+	public String getData(){
+		return data;
+	}
+
+	public void setValor(String novoValor){
+		this.valor = novoValor;
+	}
+
+	public double getValor(){
+		return valor;
+	}
+
+	public void setId(String novoId){
+		this.id = novoId;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setParcelas(String novoParcelas){
+		this.parcelas = novoParcelas;
+	}
+
+	public int getParcelas(){
+		return parcelas;
+	}
 }
 
 //Criar grupo do git
