7d56d4425905c8c02a07c1abeb7191537d3be4fb
Alecxannder-Sonnenberg <alecxannder.jsbf@usp.br>
2019-06-16 12:57:46 -0300

    Add files via upload

---
+++ Despesa.java
@@ -0,0 +1,7 @@
+public class Despesa extends Transacoes {
+
+	public Despesa(String cat, String data, double Valor, int par) {
+		super(cat, data, Valor, par);
+		// TODO Auto-generated constructor stub
+	}
+}
