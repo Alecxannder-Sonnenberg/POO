f4402912592e500393e6d930bfb26c8296c5bcff
Alecxannder-Sonnenberg <alecxannder.jsbf@usp.br>
2019-06-18 15:50:57 -0300

    Delete Despesa.java

--- Despesa.java
+++
@@ -1,7 +0,0 @@
-public class Despesa extends Transacoes {
-
-	public Despesa(String cat, String data, double Valor, int par) {
-		super(cat, data, Valor, par);
-		// TODO Auto-generated constructor stub
-	}
-}
