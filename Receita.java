14547ad290ba2c2b0f9d16e49a59449af4093210
Alecxannder-Sonnenberg <alecxannder.jsbf@usp.br>
2019-06-18 15:50:47 -0300

    Delete Receita.java

--- Receita.java
+++
@@ -1,8 +0,0 @@
-public class Receita extends Transacoes {
-
-	public Receita(String cat, String data, double Valor, int par) {
-		super(cat, data, Valor, par);
-		// TODO Auto-generated constructor stub
-	}
-
-}
